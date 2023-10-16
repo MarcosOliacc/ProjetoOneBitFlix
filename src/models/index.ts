1ebbeccbd2b1ce96ee9dad7bee84fa4cb17548db
Marcos Vinicius <marcosoliacc@gmail.com>
2023-10-16 17:19:45 -0300

    New Table and relationship between them

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,14 @@
 import { Category } from "./Category"
-import { Course } from "./course"
+import { Course } from "./Course"
+import { Epsode } from "./Episode"
 
 Category.hasMany(Course)
 Course.belongsTo(Category)
+Course.hasMany(Epsode)
+Epsode.belongsTo(Course)
 
 export {
     Category,
-    Course
+    Course,
+    Epsode
 }
