128c50b0371122f4b916ed0531e46e5dbc43e8ec
Marcos Vinicius <marcosoliacc@gmail.com>
2023-10-18 10:30:22 -0300

    Geting any course with yours episodes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { categoriesController } from "./controllers/categoriesControl"
+import { coursesController } from "./controllers/coursesControl"
 
 const router = express.Router()
 
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
+router.get('/courses/:id', coursesController.show)
 
 export { router }
