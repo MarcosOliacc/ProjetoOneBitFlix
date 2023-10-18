f61ca4a4e8002e31a93fc3f0467651ba6544ba65
Marcos Vinicius <marcosoliacc@gmail.com>
2023-10-17 22:02:20 -0300

    Upload Controllers

--- src/controllers/categoriesControl.ts
+++ src/controllers/categoriesControl.ts
@@ -1,19 +1,29 @@
 import { Request, Response } from "express";
-import { Category } from "../models";
+import { categoryService } from "../services/categoryService";
+import { getPaginatedParams } from "../helpers/getPaginatedParams";
 
 export const categoriesController = {
+    // GET /categories 
     index:async (req:Request, res:Response) => {
+        const [ page, perPage ] = getPaginatedParams(req.query)
         try {
-            const categories = await Category.findAll({
-                attributes: ['id', 'name', 'position'],
-                order: [['position', 'ASC']]
-            })
+            const categories = await categoryService.findAllPaginated(page, perPage)
             res.json(categories)            
         } catch (err) {
             if (err instanceof Error) {
                 return res.status(400).json({ message: err.message })
             }
         }
+    },
+    // GET /categories/:id
+    show: async (req: Request, res: Response) => {
+        const { id } = req.params
 
+        try {
+            
+        } catch (err) {
+            
+        }
     }
+
 }
