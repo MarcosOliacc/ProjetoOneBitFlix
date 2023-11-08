0eee4895cbca1f298cd9cde0eb3bb6a9fd2e399f
Marcos Vinicius <marcosoliacc@gmail.com>
2023-11-08 20:03:53 -0300

    Update episodeService.ts

--- src/services/episodeService.ts
+++ src/services/episodeService.ts
@@ -10,21 +10,7 @@ export const episodeService = {
         try {
             const fileStat = fs.statSync(filePath)
 
-            if(range){
-                const parts = range.replace(/bytes=/, '').split('-')
-
-                const start = parseInt(parts[0], 10)
-                const end = parts[1] ? parseInt(parts[1], 10) : fileStat.size - 1
-
-                const chunkSize = (end-start) + 1
-
-                const file = fs.createReadStream(filePath,{ start, end })
-
-                const head = {
-                    'Content-Range': `bytes ${start}-${end}/${fileStat.size}`,
-                    'Accept-Ranges': 'bytes',
-                    'Content-Length': chunkSize,
-                    'Content-Type': 'video/mp4'
+             'video/mp4'
                 }
                 res.writeHead(206,head)
 
